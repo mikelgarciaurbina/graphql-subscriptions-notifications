a7e8b704b2ff89ec07e57fef1c7bca0536c1ebf6
Mikel Garcia <mikel_6@hotmail.com>
2018-05-26 10:41:28 +0200

    Add subscription with WebSockets

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const { makeExecutableSchema } = require('graphql-tools');
 
 const { PubSub } = require('graphql-subscriptions');
 
+const cors = require('cors');
+const { execute, subscribe } = require('graphql');
+const { createServer } = require('http');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+
 const pubsub = new PubSub();
 const NOTIFICATION_SUBSCRIPTION_TOPIC = 'newNotifications';
 
@@ -13,6 +18,7 @@ const typeDefs = `
   type Query { notifications: [Notification] }
   type Notification { label: String }
   type Mutation { pushNotification(label: String!): Notification }
+  type Subscription { newNotification: Notification }
 `;
 const resolvers = {
   Query: { notifications: () => notifications },
@@ -28,15 +34,35 @@ const resolvers = {
   },
   Subscription: {
     newNotification: {
-      subscribe: () => pubsub.asyncIterator(NOTIFICATION_SUBSCRIPTION_TOPIC)
-    }
+      subscribe: () => pubsub.asyncIterator(NOTIFICATION_SUBSCRIPTION_TOPIC),
+    },
   },
 };
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 const app = express();
+app.use('*', cors({ origin: `http://localhost:3000` }));
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-app.listen(4000, () => {
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: '/graphql',
+    subscriptionsEndpoint: `ws://localhost:4000/subscriptions`,
+  }),
+);
+const ws = createServer(app);
+ws.listen(4000, () => {
   console.log('Go to http://localhost:4000/graphiql to run queries!');
+
+  new SubscriptionServer(
+    {
+      execute,
+      subscribe,
+      schema,
+    },
+    {
+      server: ws,
+      path: '/subscriptions',
+    },
+  );
 });
